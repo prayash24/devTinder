d658f618b4f01ba34b45777f073bbea3c26cd76a
prayash24 <112775887+prayash24@users.noreply.github.com>
2026-02-09 22:50:18 +0530

    exploring get post dlt

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+//this will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "prayash", lastName: "naik"})
+});
+
+app.post("/user", (req, res) => {
+    //saving data to db
+    res.send("Data saved!");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data Deleted");
+});
+
+//this will match all the http method api calls to /
 app.use("/", (req, res) => {
     res.send("Namaste Dev")
 });
@@ -10,6 +25,6 @@ app.use("/test", (req, res)=>{
 
 });
 
-app,listen(7777, ()=>{
+app.listen(7777, ()=>{
     console.log("server running");
 });
